933a3976c618b41d9cce6c3b4e21830122471377
段体华 <duantihua@gmail.com>
2026-08-26 08:14:07 +0800

    remove multiple js loadding style

--- static/src/main/resources/js/jquery-pstrength.js
+++ static/src/main/resources/js/jquery-pstrength.js
@@ -215,3 +215,5 @@ jQuery.extend(jQuery.fn.pstrength, {
     return true;
   }
 });
+digitalspaghetti.password.defaults.minCharText='至少%d个字符';
+digitalspaghetti.password.defaults.verdicts=['弱', '正    常', '中   度', '强!', '很   强!'];
